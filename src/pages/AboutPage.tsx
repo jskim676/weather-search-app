c6d5b874f51c04117bead8cb054ea318dcf1b47a
jskim676 <wkxndlsvk@gmail.com>
2025-06-23 18:08:01 +0900

    #2 폴더 구조 생성 및 기본 라우터 설정

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,9 @@
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>이 앱은 대한민국 날씨 정보를 제공합니다</h1>
+        </main>
+    );
+};
+
+export default AboutPage;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+    return (
+        <main>
+            <h1>날씨 앱 시작 페이지</h1>
+        </main>
+    );
+};
+
+export default HomePage;
